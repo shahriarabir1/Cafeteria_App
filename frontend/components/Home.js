4a4f01361ec1fe9a38c6775da37790c3ef2c0304
shahriarabir1 <shahriarabirjob@gmail.com>
2023-10-31 23:46:49 +0600

    second

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-
-import React from "react";
+import { Button, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Home = () => {
   return (
     <View>
-      <Text>It is home page</Text>
+      <View style={{ backgroundColor: "blue", height: "100%", width: "100vw" ,display:'flex',borderCurve:'5px'}}>
+        <View>
+          <Icon name="navicon" size={30} color="#900" />
+        </View>
+      </View>
     </View>
   );
 };
