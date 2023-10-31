12f87950485ac00e31942709a024e1188cfe02b0
shahriarabir1 <shahriarabirjob@gmail.com>
2023-10-31 16:15:08 +0600

    second

---
+++ frontend/components/Home.js
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>It is home page</Text>
+    </View>
+  );
+};
+
+export default Home;
